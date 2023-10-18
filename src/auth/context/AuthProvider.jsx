0f22e5c5e22deec17878e9b8b3b9b0269bddcec0
JonathanArriazu <joni.arriazu2@gmail.com>
2023-10-18 18:01:08 -0300

    feat: AuthContext and AuthProvider implemented

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import React, { useReducer } from 'react'
+import { AuthContext } from './AuthContext'
+import { AuthReducer } from './AuthReducer'
+
+const initialState = {
+    logged: false
+}
+
+export const AuthProvider = ({children}) => {
+
+    const [ authState, dispatch ] = useReducer( AuthReducer, initialState );
+
+    return (
+        <AuthContext.Provider value={{}}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
---
+++ src/auth/context/AuthReducer.jsx
@@ -0,0 +1,26 @@
+import { types } from "../types/types";
+
+const initialState = {
+    login: false
+}
+
+
+export const AuthReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case types.login:
+            return {
+                ...state, // => hacemos esto por si existen mas propiedades en nuestro estado
+                logged: true,
+                name: action.payload
+            }
+        
+        case types.logout:
+            return {
+                logged: false
+            }
+        default:
+            return state;
+    }
+
+}
