21f33e8689be6ca567936b6df247047047868548
JonathanArriazu <joni.arriazu2@gmail.com>
2023-10-18 13:28:31 -0300

    feat: Hero Card Added

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const HeroCard = ({
+    id, 
+    superhero, 
+    publisher, 
+    alter_ego, 
+    first_apperance, 
+    characters
+}) => {
+
+    const heroImageUrl = `assets/heroes/${id}.jpg`
+        
+    return (
+        <div className="col">
+            <div className="card">
+
+                <div className='row no-glutters'>
+
+                    <div className="col-4">
+                        <img src={ heroImageUrl } className='card-img' alt={ superhero } />
+                    </div>
+
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className='card-title'>{ superhero }</h5>
+                            <p className='card-text'>{ alter_ego }</p>
+                            <p>
+                                { (alter_ego !== characters) && (<p>{ characters }</p>) }
+                            </p>
+                            <p className='card-text'>
+                                <small className='text-muted'>{ first_apperance }</small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}> Ver más... </Link>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { getHeroesbyPublisher } from '../helpers/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesbyPublisher(publisher);
     
   return (
-    <ul>
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
         {heroes.map( (hero) => (
-            <li 
-                key={hero.id}
-            >
-                {hero.superhero}
-            </li>
+            <HeroCard key={hero.id} {...hero}/> // => {...hero} sirve para desestructurar al hero, en lugar de estar colocando cada una de sus propiedades por separado
         ))}
 
-    </ul>
+    </div>
 
   )
 }
